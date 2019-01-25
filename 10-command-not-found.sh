e109c952d1d81903e5f14dc3a53b4ff1147c0dab
Auke Kok <auke-jan.h.kok@intel.com>
2019-01-25 10:39:31 -0800

    Differentiate user message.
    If root, display command.
    If wheel, display sudo command.
    Else, display command for admin to execute.

--- 10-command-not-found.sh
+++ 10-command-not-found.sh
@@ -1,5 +1,12 @@
 command_not_found_handle () {
     echo "$1: command not found"
-    awk -F"\t" -v cmd="$1" '$1 == cmd { print "To install " $1 " use:  swupd bundle-add " $2; exit }' /usr/share/clear/commandlist.csv
+    if [ "`id -u`" == "0" ]; then
+        MSG='$1 == cmd { print "To install " $1 " use:  swupd bundle-add " $2; exit }'
+    elif groups | grep -q -w -e wheel -e wheelnopw ; then
+        MSG='$1 == cmd { print "To install " $1 " use:  sudo swupd bundle-add " $2; exit }'
+    else
+        MSG='$1 == cmd { print "To install " $1 " your system administrator needs to do:  swupd bundle-add " $2; exit }'
+    fi
+    awk -F"\t" -v cmd="$1" "$MSG" /usr/share/clear/commandlist.csv
     return 127
 }
