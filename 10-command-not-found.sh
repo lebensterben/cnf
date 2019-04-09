f85f20b162eade818aaef86dba3bdc10c9075bd0
Patrick McCarty <patrick.mccarty@intel.com>
2019-04-09 12:13:02 -0700

    When sudo is missing, fall through to sysadmin instructions
    If `sudo` is missing, print the sysadmin instructions, not the
    instructions requiring presence of `sudo`.
    
    Fixes https://github.com/clearlinux/distribution/issues/457
    
    Signed-off-by: Patrick McCarty <patrick.mccarty@intel.com>

--- 10-command-not-found.sh
+++ 10-command-not-found.sh
@@ -2,7 +2,7 @@ command_not_found_handle () {
     echo "$1: command not found"
     if [ "`id -u`" == "0" ]; then
         MSG='$1 == cmd { print "To install " $1 " use:  swupd bundle-add " $2; exit }'
-    elif groups | grep -q -w -e wheel -e wheelnopw ; then
+    elif [ "$1" != "sudo" ] && groups | grep -q -w -e wheel -e wheelnopw ; then
         MSG='$1 == cmd { print "To install " $1 " use:  sudo swupd bundle-add " $2; exit }'
     else
         MSG='$1 == cmd { print "To install " $1 " your system administrator needs to do:  swupd bundle-add " $2; exit }'
