91e981e4bad8263eea00e5e93b2dc130b41f566c
Lucius Hu <orctarorga@gmail.com>
2019-06-05 11:41:32 -0500

    Command-not-found also works for zsh now
    This is a patch for `cnf` package.
    
    `10-command-not-found.sh` is located in `/usr/share/defaults/etc/profile.d`,
    and is sourced by start files of `bash`.
    
    As I intend to unifying startup files of both `bash` and `zsh`, it would
    also be sourced by `zsh`, if my suggestion is accepted.
    
    It works for `zsh` now with the following modificatiosn:
    
    - This functionality shall only be enabled for interactive shell, and is
      tested by `[ "$(expr $- : '.*i')" -ne 0 ]`.
      Though this test expression looks ugly, it is POSIX compatible and doesn't
      require external program.
    
    - I replaced the obsolescent ` `foo bar` ` syntax with ` $(foo bar) `.
    
    - The test of UID was `[ "`id -u`" == "0" ]`, where `==` is not
      Bourne-compatible and doesn't work for `zsh` unless I replace the
      `[` with `[[`. But `[[` itself is also not Bourne-compatible. So I
      decide to have `[ "$(id -u)" = "0" ]` instead, which works for
      Bourne-compatible shells.
    
    - The `command_not_found_handle` is loaded as long as it's interactive
      shell, and is used directly by `bash`. When
      it's `zsh` shell, then a wrapper function `command_not_found_handler`
      is defined.

--- 10-command-not-found.sh
+++ 10-command-not-found.sh
@@ -1,13 +1,14 @@
-command_not_found_handle () {
+if [ "$(expr $- : '.*i')" -ne 0 ]; then
+  command_not_found_handle () {
     echo "$1: command not found"
-    if [ "`id -u`" == "0" ]; then
+    if [ "$(id -u)" = "0" ]; then
         MSG='$1 == cmd { print "To install " $1 " use:  swupd bundle-add " $2; exit }'
     elif [ "$1" != "sudo" ] && groups | grep -q -w -e wheel -e wheelnopw ; then
         MSG='$1 == cmd { print "To install " $1 " use:  sudo swupd bundle-add " $2; exit }'
     else
         MSG='$1 == cmd { print "To install " $1 " your system administrator needs to do:  swupd bundle-add " $2; exit }'
     fi
-    R=`awk -F"\t" -v cmd="$1" "$MSG" /usr/share/clear/commandlist.csv`
+    R=$(awk -F"\t" -v cmd="$1" "$MSG" /usr/share/clear/commandlist.csv)
     if [ -n "$R" ]; then
         echo $R
     else
@@ -15,4 +16,12 @@ command_not_found_handle () {
         awk -F"\t" -v cmd="$1" "$MSG" /usr/share/clear/alternatives.csv
     fi
     return 127
-}
+  }
+
+  if [ "$ZSH_VERSION" ]; then
+    command_not_found_handler () {
+      command_not_found_handle "$@"
+      return $?
+    }
+  fi
+fi
